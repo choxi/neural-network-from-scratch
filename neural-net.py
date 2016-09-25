193d5107ea6abc8f48adb2139e12e80ae85da5d5
Roshan Choxi <roshan.choxi@gmail.com>
2016-09-24 23:19:25 -0500

    zeroes -> zeros

--- neural-net.py
+++ neural-net.py
@@ -51,9 +51,9 @@ def build_model(nn_hdim, num_passes=20000, print_loss=False):
     # TODO: why do we divide the weights by the square root of the input size?
     np.random.seed(0)
     W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
-    b1 = np.zeroes((1, nn_hdim))
+    b1 = np.zeros((1, nn_hdim))
     W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
-    b2 = np.zeroes((1, nn_output_dim))
+    b2 = np.zeros((1, nn_output_dim))
 
     model = {}
 
