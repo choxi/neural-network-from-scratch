b755692d956bb37f03817e03de554ab140b85686
Roshan Choxi <roshan.choxi@gmail.com>
2016-09-24 23:25:59 -0500

    outcome -> probs

--- neural-net.py
+++ neural-net.py
@@ -20,13 +20,12 @@ def calculate_loss(model):
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
 
     # Forward propogation
-    z1 = X.dot(W1) + b1
-    a1 = np.tanh(z1)
-    z2 = a1.dot(W2) + b2
-    output = softmax(z2)
+    z1    = X.dot(W1) + b1
+    a1    = np.tanh(z1)
+    z2    = a1.dot(W2) + b2
+    probs = softmax(z2)
 
     # Calculate loss
-    # TODO: what is "probs"?
     correct_logprobs = -np.log(probs[range(num_examples)])
     data_loss        = np.sum(correct_logprobs)
 
